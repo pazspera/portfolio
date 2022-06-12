becf5e05fe3c4ef2cbeafd83dc0a065677be916c
Paz Spera <mspera03@gmail.com>
2022-06-11 21:28:55 -0300

    agregué validaciones de formulario con campos de error dinámicos

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const d = document,
+  $form = d.querySelector(".contact-form"),
+  $inputs = d.querySelectorAll(".contact-form [required]");
+
+$inputs.forEach((input) => {
+  const $span = d.createElement("span");
+  $span.id = input.name;
+  $span.textContent = input.title;
+  $span.classList.add("contact-form-error", "none");
+  input.insertAdjacentElement("afterend", $span);
+});
+
+d.addEventListener("keyup", (e) => {
+  if (e.target.matches(".contact-form [required]")) {
+    let $input = e.target,
+      pattern = $input.pattern || $input.dataset.pattern;
+
+    if (pattern && $input.value !== "") {
+      let regex = new RegExp(pattern);
+      return !regex.exec($input.value) ? d.getElementById($input.name).classList.add("is-active") : d.getElementById($input.name).classList.remove("is-active");
+    }
+  }
+});
