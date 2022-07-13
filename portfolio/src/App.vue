253a61fe4273351dbe0c6412940544cbbf1a6ca5
Paz Spera <mspera03@gmail.com>
2022-07-12 23:38:25 -0300

    agregué estilos iniciales scss

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-9">
+          <img alt="Vue logo" src="./assets/logo.png" />
+          <HelloWorld msg="Welcome to Your Vue.js App" />
+        </div>
+        <div class="col-3">
+          <h1>Hola</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "@/assets/scss/main.scss";
 </style>
