f21e54c2b88664d289e8558100c69ae02ff5590a
Paz Spera <mspera03@gmail.com>
2022-07-12 23:25:47 -0300

    agregué proyecto inicial vue

---
+++ portfolio/src/main.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
