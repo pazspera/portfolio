2f12ac5a53f978b80be0b23eead5175eeea1d337
Paz Spera <mspera03@gmail.com>
2022-07-21 02:00:15 -0300

    armé el template del ProjectComponent y corregí ProjectView para que el ProjectComponent quede bien dentro de la grilla de bootstrap

--- portfolio/src/components/ProjectComponent.vue
+++ portfolio/src/components/ProjectComponent.vue
@@ -1,29 +1,22 @@
 <template>
-  <div>
-    <div class="row mb-5">
-      <div class="col-12 col-lg-6">
-        <img :="imgViernes" />
+  <div class="row mb-5">
+    <div class="col-12 col-lg-6">
+      <img :="project.image" />
+    </div>
+    <div class="col-12 col-lg-5 offset-lg-1">
+      <div class="mb-5">
+        <h3 class="mb-3">{{ project.title }}</h3>
+        <p class="proyects__text">{{ project.description }}</p>
+        <p class="proyects__text"><strong>Rol:</strong> {{ project.role }}</p>
       </div>
-      <div class="col-12 col-lg-5 offset-lg-1">
-        <div class="mb-5">
-          <h3 class="mb-3">Viernes</h3>
-          <p class="proyects__text">
-            Sitio web del ciclo de cine de los Viernes en La casa del Craizy. Este es un proyecto personal muy querido que lleva la crónica de las películas compartidas con amigxs durante la pandemia.
-          </p>
-          <p class="proyects__text"><strong>Rol:</strong> Diseñadora UI, desarrolladora frontend</p>
-        </div>
-        <h4 class="mb-3 stack--title">Desarrollado con:</h4>
-        <div class="stack">
-          <i class="devicon-html5-plain proyect-icon"></i>
-          <i class="devicon-sass-original proyect-icon"></i>
-          <i class="devicon-bootstrap-plain proyect-icon"></i>
-          <i class="devicon-javascript-plain proyect-icon"></i>
-        </div>
-        <div class="btns my-3">
-          <a href="https://pazspera.github.io/viernes-web/index.html" class="btn btn__secondary" rel="noreferrer" target="_blank">Sitio</a>
-          <a href="https://github.com/pazspera/viernes-web" class="btn btn__secondary ms-3" rel="noreferrer" target="_blank">Repositorio Github</a>
-        </div>
+      <h4 class="mb-3 stack--title">Desarrollado con:</h4>
+      <div class="stack">
+        <span v-for="tech in project.techStack" :key="tech" v-html="tech"></span>
       </div>
+      <div class="btns my-3">
+        <a v-for="btn in project.btns" :key="btn" :href="btn.link" class="btn btn__secondary me-3" rel="noreferrer" target="_blank">{{ btn.name }}</a>
+      </div>
+      <div class="spacer"></div>
     </div>
   </div>
 </template>
@@ -31,6 +24,11 @@
 <script>
 export default {
   name: "ProjectComponent",
+  props: {
+    project: {
+      type: Object,
+    },
+  },
 };
 </script>
 
--- portfolio/src/views/ProjectsView.vue
+++ portfolio/src/views/ProjectsView.vue
@@ -4,94 +4,28 @@
       <div class="container">
         <div class="row">
           <div class="col mb-5">
-            <h2>Proyectos</h2>
-          </div>
-        </div>
-        <!-- Viernes -->
-        <div class="row mb-5">
-          <div class="col-12 col-lg-6">
-            <img :="imgViernes" />
-          </div>
-          <div class="col-12 col-lg-5 offset-lg-1">
-            <div class="mb-5">
-              <h3 class="mb-3">Viernes</h3>
-              <p class="proyects__text">
-                Sitio web del ciclo de cine de los Viernes en La casa del Craizy. Este es un proyecto personal muy querido que lleva la crónica de las películas compartidas con amigxs durante la
-                pandemia.
-              </p>
-              <p class="proyects__text"><strong>Rol:</strong> Diseñadora UI, desarrolladora frontend</p>
-            </div>
-            <h4 class="mb-3 stack--title">Desarrollado con:</h4>
-            <div class="stack">
-              <i class="devicon-html5-plain proyect-icon"></i>
-              <i class="devicon-sass-original proyect-icon"></i>
-              <i class="devicon-bootstrap-plain proyect-icon"></i>
-              <i class="devicon-javascript-plain proyect-icon"></i>
-            </div>
-            <div class="btns my-3">
-              <a href="https://pazspera.github.io/viernes-web/index.html" class="btn btn__secondary" rel="noreferrer" target="_blank">Sitio</a>
-              <a href="https://github.com/pazspera/viernes-web" class="btn btn__secondary ms-3" rel="noreferrer" target="_blank">Repositorio Github</a>
-            </div>
-          </div>
-        </div>
-        <!-- Hot Rod Records -->
-        <div class="spacer"></div>
-        <div class="row mb-5">
-          <div class="col-12 col-lg-6">
-            <img :="imgHotRod" />
-          </div>
-          <div class="col-12 col-lg-5 offset-lg-1">
-            <div class="mb-5">
-              <h3 class="mb-3">Hot Rod Records</h3>
-              <p class="proyects__text">Ecommerce de una tienda de música especializada en rock 'n' roll. Vinilos, Elvis Presley y WordPress.</p>
-              <p class="proyects__text"><strong>Rol:</strong> Diseñadora UI, desarrolladora, redactora de contenido</p>
-            </div>
-            <h4 class="mb-3 stack--title">Desarrollado con:</h4>
-            <div class="stack">
-              <i class="devicon-wordpress-plain proyect-icon"></i>
-              <i class="devicon-woocommerce-plain-wordmark proyect-icon"></i>
-            </div>
-            <div class="btns my-3">
-              <a href="https://hotrodrecords.tk/" class="btn btn__secondary" rel="noreferrer" target="_blank">Sitio</a>
-            </div>
-          </div>
-        </div>
-        <!-- Kaio Sushi -->
-        <div class="spacer"></div>
-        <div class="row mb-5">
-          <div class="col-12 col-lg-6">
-            <img :="imgKaio" />
-          </div>
-          <div class="col-12 col-lg-5 offset-lg-1">
-            <div class="mb-5">
-              <h3 class="mb-3">Kaio Sushi</h3>
-              <p class="proyects__text">Ecommerce de un local de venta de sushi. El proyecto incluye un carrito funcional desarrollado mayormente con Vanilla JavaScript y unos detalles de jQuery.</p>
-              <p class="proyects__text"><strong>Rol:</strong> Diseñadora UI, desarrolladora frontend</p>
-            </div>
-            <h4 class="mb-3 stack--title">Desarrollado con:</h4>
-            <div class="stack">
-              <i class="devicon-html5-plain proyect-icon"></i>
-              <i class="devicon-sass-original proyect-icon"></i>
-              <i class="devicon-bootstrap-plain proyect-icon"></i>
-              <i class="devicon-javascript-plain proyect-icon"></i>
-              <i class="devicon-jquery-plain proyect-icon"></i>
-            </div>
-            <div class="btns my-3">
-              <a href="https://pazspera.github.io/kaio-sushi/" class="btn btn__secondary" rel="noreferrer" target="_blank">Sitio</a>
-              <a href="https://github.com/pazspera/kaio-sushi" class="btn btn__secondary ms-3" rel="noreferrer" target="_blank">Repositorio Github</a>
-            </div>
+            <h2>Prueba componente</h2>
           </div>
         </div>
       </div>
+
+      <div class="container" v-for="project in projects" :key="project.title">
+        <ProjectComponent :project="project" />
+      </div>
+
     </section>
   </div>
 </template>
 
 <script>
 import json from "@/assets/data/projects.json";
+import ProjectComponent from "@/components/ProjectComponent.vue";
 
 export default {
   name: "ProjectsView",
+  components: {
+    ProjectComponent,
+  },
   mounted() {
     document.title = "Proyectos - Paz Spera";
     console.log(this.projects);
