da90b15eb828b1804ba864bf06923cb135bddd6d
Paz Spera <mspera03@gmail.com>
2022-11-04 01:36:23 -0300

    Animaciones AboutView

--- portfolio/src/views/AboutView.vue
+++ portfolio/src/views/AboutView.vue
@@ -8,67 +8,73 @@
       </div>
       <div class="row">
         <div class="col-10 col-lg-4">
-          <img :="imagePaz" class="image--shadow mb-5" />
+          <transition appear name="fadeLeft">
+            <img :="imagePaz" class="image--shadow mb-5" />
+          </transition>
         </div>
         <div class="col col-lg-7 offset-lg-1">
-          <p class="about__text">¡Hola Mundo! Soy Paz Spera, desarrolladora frontend y diseñadora UI de Buenos Aires, Argentina.</p>
-          <p class="about__text">
-            Más allá de la innovación técnica, la tecnología nos ofrece constantemente nuevas formas de comunicarnos. Combinar un código organizado y funcional con un diseño atractivo y accesible es
-            donde comienza la magia.
-          </p>
+          <transition appear name="fadeRight">
+            <div>
+              <p class="about__text">¡Hola Mundo! Soy Paz Spera, desarrolladora frontend y diseñadora UI de Buenos Aires, Argentina.</p>
+              <p class="about__text">
+                Más allá de la innovación técnica, la tecnología nos ofrece constantemente nuevas formas de comunicarnos. Combinar un código organizado y funcional con un diseño atractivo y accesible
+                es donde comienza la magia.
+              </p>
 
-          <!-- Habilidades -->
-          <h3 class="mt-5 mb-3">Habilidades</h3>
-          <div class="skills-grid pt-4">
-            <div class="skill-item">
-              <i class="devicon-html5-plain"></i>
-              <span>HTML</span>
+              <!-- Habilidades -->
+              <h3 class="mt-5 mb-3">Habilidades</h3>
+              <div class="skills-grid pt-4">
+                <div class="skill-item">
+                  <i class="devicon-html5-plain"></i>
+                  <span>HTML</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-css3-plain"></i>
+                  <span>CSS</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-javascript-plain"></i>
+                  <span>JavaScript</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-jquery-plain"></i>
+                  <span>jQuery</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-vuejs-plain"></i>
+                  <span>Vue</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-bootstrap-plain"></i>
+                  <span>Bootstrap</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-sass-original"></i>
+                  <span>Sass</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-git-plain"></i>
+                  <span>Git</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-wordpress-plain"></i>
+                  <span>WordPress</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-woocommerce-plain-wordmark"></i>
+                  <span>WooCommerce</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-figma-plain"></i>
+                  <span>Figma</span>
+                </div>
+                <div class="skill-item">
+                  <i class="devicon-photoshop-plain"></i>
+                  <span>Photoshop</span>
+                </div>
+              </div>
             </div>
-            <div class="skill-item">
-              <i class="devicon-css3-plain"></i>
-              <span>CSS</span>
-            </div>
-            <div class="skill-item">
-              <i class="devicon-javascript-plain"></i>
-              <span>JavaScript</span>
-            </div>
-            <div class="skill-item">
-              <i class="devicon-jquery-plain"></i>
-              <span>jQuery</span>
-            </div>
-            <div class="skill-item">
-              <i class="devicon-vuejs-plain"></i>
-              <span>Vue</span>
-            </div>
-            <div class="skill-item">
-              <i class="devicon-bootstrap-plain"></i>
-              <span>Bootstrap</span>
-            </div>
-            <div class="skill-item">
-              <i class="devicon-sass-original"></i>
-              <span>Sass</span>
-            </div>
-            <div class="skill-item">
-              <i class="devicon-git-plain"></i>
-              <span>Git</span>
-            </div>
-            <div class="skill-item">
-              <i class="devicon-wordpress-plain"></i>
-              <span>WordPress</span>
-            </div>
-            <div class="skill-item">
-              <i class="devicon-woocommerce-plain-wordmark"></i>
-              <span>WooCommerce</span>
-            </div>
-            <div class="skill-item">
-              <i class="devicon-figma-plain"></i>
-              <span>Figma</span>
-            </div>
-            <div class="skill-item">
-              <i class="devicon-photoshop-plain"></i>
-              <span>Photoshop</span>
-            </div>
-          </div>
+          </transition>
         </div>
       </div>
     </div>
@@ -131,5 +137,27 @@ export default {
   grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
   gap: 16px;
   grid-auto-flow: row dense;
+}
+
+/* Transition styles */
+.fadeLeft-enter-from {
+  opacity: 0;
+  transform: translateX(-200px);
+}
+
+.fadeLeft-enter-to,
+.fadeRight-enter-to {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.fadeRight-enter-from {
+  opacity: 0;
+  transform: translateX(200px);
+}
+
+.fadeLeft-enter-active,
+.fadeRight-enter-active {
+  transition: all 0.45s ease-in;
 }
 </style>
