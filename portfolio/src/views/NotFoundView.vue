eb715084bce63d2b41abfa313ce10fa0fcecbfb1
Paz Spera <mspera03@gmail.com>
2022-12-21 00:28:47 -0300

    Cambios a estilo de NotFoundView, borré párrafo y agrandé img. Nuevo build con esos cambios

--- portfolio/src/views/NotFoundView.vue
+++ portfolio/src/views/NotFoundView.vue
@@ -2,9 +2,8 @@
   <section class="not-found">
     <div class="container">
       <div class="col d-flex flex-column justify-content-center align-items-center col-not-found">
-        <img :="errorImg" />
+        <img :="errorImg" class="mb-4" />
         <h1 class="mb-3">¡Oh, no! La página no está disponible</h1>
-        <p>Pero nunca es tarde para volver a empezar.</p>
         <router-link :to="{ name: 'home' }" class="btn btn__primary mt-4">Volver al inicio</router-link>
       </div>
     </div>
