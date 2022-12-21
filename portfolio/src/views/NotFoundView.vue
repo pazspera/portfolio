2f4cfccb4a1b915b3e37c39ae19154b740acd2f6
Paz Spera <mspera03@gmail.com>
2022-12-20 23:25:45 -0300

    Agregué contenido a NotFoundView2

--- portfolio/src/views/NotFoundView.vue
+++ portfolio/src/views/NotFoundView.vue
@@ -1,4 +1,13 @@
 <template>
+  <section>
+    <div class="container d-flex justify-content-center align-items-center">
+      <div class="col">
+        <h1>¡Oh, no! La página no está disponible</h1>
+        <p>Pero nunca es tarde para volver a empezar.</p>
+        <router-link :to="{ name: 'home'}" class="btn btn__primary">Volver al inicio</router-link>
+      </div>
+    </div>
+  </section>
   <h1>404</h1>
   <h2>Page not found</h2>
 </template>
