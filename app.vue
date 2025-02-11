24909f95edfe89a0d90b508c3a38d95e8fb4d9d5
Paz Spera <mspera03@gmail.com>
2025-02-11 17:51:49 -0300

    Agregado Bootstrap, Sass, Nuxt Content

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="danger">Hola Mundo</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  import('bootstrap/dist/js/bootstrap.bundle.min.js');
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import {defineNuxtConfig} from "nuxt/config";
+import {resolve} from "path";
+
+// https://nuxt.com/docs/api/ configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  css: ["~/assets/main.scss"],
+  modules: ['@nuxt/content'],
 })
