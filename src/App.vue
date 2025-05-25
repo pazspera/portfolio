d9e3159f2b8ccdffa42cab11cad67fbfe2252db8
Paz Spera <mspera03@gmail.com>
2025-05-25 20:33:18 -0300

    Agregada fuente logo. Creado componente Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
+  <Navbar />
   <h1 class="font-title color-primary-900">Hello World!</h1>
   <div class="w-20 h-20 bg-primary-500"></div>
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="bg-primary-800 w-full h-16">
+    <nav></nav>
+  </header>
+</template>
