6f2ac84779234d1d1163418332cae8f620b7d951
Paz Spera <mspera03@gmail.com>
2025-05-25 14:04:10 -0300

    Agregadas tipografías de marca. tailwind.config.ts no está siendo necesario, se maneja desde el css ok

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <h1 class="font-title color-primary-900">Hello World!</h1>
+  <div class="w-20 h-20 bg-primary-500"></div>
 </template>
 
 <style scoped>
