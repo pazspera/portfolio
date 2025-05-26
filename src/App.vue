3b5c4fdf30091503b51e42d6a50feee68ae3e02a
Paz Spera <mspera03@gmail.com>
2025-05-26 16:25:05 -0300

    Cambio en Navlogo para que no tenga px, así el tamaño del container es consistente en el resto de la app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue';
+import Hero from './sections/Hero.vue';
 </script>
 
 <template>
   <Navbar />
   <main class="bg-light dark:bg-dark text-zinc-900 dark:text-zinc-200">
     <div class="container mx-auto py-8 px-4 md:px-0">
+      <Hero />
       <h1 class="font-title color-primary-900">Hello World!</h1>
       <div class="w-20 h-20 bg-primary-500"></div>
     </div>    
---
+++ src/sections/Hero.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="bg-primary-200 dark:bg-primary-400 px-4 py-6">
+    <div class="container mx-auto">
+      <p class="text-sm">¡Hola, soy Paz! Me especializo en</p>
+      <h1 class="font-title text-4xl">Captar la atención, guiar al usuario y lograr conversiones</h1>
+      <p class="text-lg">Cuando el diseño, el código y las palabras trabajan en equipo, los resultados no tardan en llegar.</p>
+    </div>
+  </section>
+</template>
