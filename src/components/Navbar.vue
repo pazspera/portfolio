d823ea8126cfa2779d6a60f6d20033ef3be5269c
Paz Spera <mspera03@gmail.com>
2025-05-26 00:57:06 -0300

    Maquetado Navbar para desktop

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
 import { Bars3Icon, SunIcon, MoonIcon } from "@heroicons/vue/24/outline";
+import { ref } from "vue";
+
+const menuOpen = ref(false);
+const toggleMenu = () => {
+  menuOpen.value = !menuOpen.value;
+}
+
 </script>
 
 <template>
@@ -7,22 +14,36 @@ import { Bars3Icon, SunIcon, MoonIcon } from "@heroicons/vue/24/outline";
     <div class="container mx-auto px-4 py-1.5 md:px-0">
       <nav class="flex justify-between items-center">
         <!-- Logo -->
-        <span class="font-logo text-2xl text-primary-100 hover:text-primary-400 transition ease-in-out">PS</span>
-        <!-- Buttons -->
-        <div class="flex items-center gap-6">
-          <button>
-            <SunIcon class="w-6 h-6 text-primary-100 hover:text-primary-400 transition ease-in-out"/>
-            <MoonIcon class="w-6 h-6 text-primary-100 hover:text-primary-400 transition ease-in-out"/>
-          </button>
-          <Bars3Icon class="w-6 h-6 text-primary-100 cursor-pointer hover:text-primary-400 transition ease-in-out md:hidden"/>
-        </div>  
+        <span class="font-logo text-2xl text-primary-100 hover:text-primary-400 hover:font-bold transition ease-in-out">PS</span>
+
+        <div class="ml-auto flex items-center gap-6">
+          <!-- Desktop links -->
+          <div class="hidden md:flex md:flex-row gap-4">
+            <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 hover:font-bold transition ease-in-out">Proyectos</a>
+            <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 hover:font-bold transition ease-in-out">Acerca de</a>
+            <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 hover:font-bold transition ease-in-out">Contacto</a>
+          </div>
+  
+          <!-- Buttons -->
+          <div class="flex items-center gap-6">
+            <button>
+              <SunIcon class="w-6 h-6 text-primary-100 hover:text-primary-400 transition ease-in-out"/>
+              <MoonIcon class="w-6 h-6 text-primary-100 hover:text-primary-400 transition ease-in-out"/>
+            </button>
+            <button @click="toggleMenu">
+              <Bars3Icon class="w-6 h-6 text-primary-100 cursor-pointer hover:text-primary-400 transition ease-in-out md:hidden"/>
+            </button>
+          </div>
+        </div>
+
       </nav>
     </div>
 
-    <div class="hidden flex-col md:flex md:flex-row items-center justify-start md:space-x-1 pb-3 md:pb-0" id="navMenu">
-      <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 transition ease-in-out">Proyectos</a>
-      <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 transition ease-in-out">Acerca de</a>
-      <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 transition ease-in-out">Contacto</a>
+    <!-- Mobile menu -->
+    <div v-if="menuOpen" class="flex-col md:flex md:flex-row items-center justify-start md:space-x-1 pb-3 md:pb-0">
+      <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 hover:font-bold transition ease-in-out">Proyectos</a>
+      <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 hover:font-bold transition ease-in-out">Acerca de</a>
+      <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 hover:font-bold transition ease-in-out">Contacto</a>
     </div>
   </header>
 </template>
