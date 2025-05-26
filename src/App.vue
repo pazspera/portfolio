2bfbd083d210c7cd18879c3500774dc259a20fec
Paz Spera <mspera03@gmail.com>
2025-05-26 00:41:55 -0300

    Maquetado Navbar. Instalado Heroicons

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar.vue';
 
 <template>
   <Navbar />
-  <div class="container mx-auto py-8">    
+  <div class="container mx-auto py-8 px-4 md:px-0">    
     <h1 class="font-title color-primary-900">Hello World!</h1>
     <div class="w-20 h-20 bg-primary-500"></div>
   </div>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
-
+import { Bars3Icon, SunIcon, MoonIcon } from "@heroicons/vue/24/outline";
 </script>
 
 <template>
   <header class="bg-primary-800 w-full h-auto ">
     <div class="container mx-auto px-4 py-1.5 md:px-0">
-      <nav class="flex">
-        <p class="font-logo text-2xl text-primary-100">PS</p>  
+      <nav class="flex justify-between items-center">
+        <!-- Logo -->
+        <span class="font-logo text-2xl text-primary-100 hover:text-primary-400 transition ease-in-out">PS</span>
+        <!-- Buttons -->
+        <div class="flex items-center gap-6">
+          <button>
+            <SunIcon class="w-6 h-6 text-primary-100 hover:text-primary-400 transition ease-in-out"/>
+            <MoonIcon class="w-6 h-6 text-primary-100 hover:text-primary-400 transition ease-in-out"/>
+          </button>
+          <Bars3Icon class="w-6 h-6 text-primary-100 cursor-pointer hover:text-primary-400 transition ease-in-out md:hidden"/>
+        </div>  
       </nav>
+    </div>
+
+    <div class="hidden flex-col md:flex md:flex-row items-center justify-start md:space-x-1 pb-3 md:pb-0" id="navMenu">
+      <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 transition ease-in-out">Proyectos</a>
+      <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 transition ease-in-out">Acerca de</a>
+      <a href="#" class="py-2 px-3 block text-primary-100  hover:text-primary-400 transition ease-in-out">Contacto</a>
     </div>
   </header>
 </template>
