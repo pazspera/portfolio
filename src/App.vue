71722b15c6f12ee590fd69de413d75840c03f224
Paz Spera <mspera03@gmail.com>
2025-05-26 15:23:01 -0300

    Creado main para que contenga toda la app en App.vue. Backup de las clases dark y text en style.css para evitar pantallazos

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import DarkToggle from './components/DarkToggle.vue';
 
 <template>
   <Navbar />
-  <main >
+  <main class="bg-light dark:bg-dark text-zinc-900 dark:text-zinc-200">
     <div class="container mx-auto py-8 px-4 md:px-0">
       <h1 class="font-title color-primary-900">Hello World!</h1>
       <div class="w-20 h-20 bg-primary-500"></div>
