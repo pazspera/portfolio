84ef947dc61803afd724a79f6498d756989b9750
Paz Spera <mspera03@gmail.com>
2025-05-27 18:28:48 -0300

    Agregado contenido a Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,12 +4,16 @@ let year = data.getFullYear();
 </script>
 
 <template>
-  <footer>
-    <div class="container mx-auto px-4 py-4 md:px-6 bg-primary-800 text-primary-100">
-      <div class="grid grid-cols-1 md:grid-cols-8">
-        <div>
+  <footer class="bg-primary-800 text-primary-100">
+    <div class="container mx-auto px-4 py-4 md:px-6 ">
+      <div class="grid grid-cols-6 ">
+        <div class="col-span-4">
           <p class="text-base md:text-lg">&#169; {{ year }} - Paz Spera</p>
           <p class="text-base md:text-lg">Hecho con ☕ + ❤️​ y un poco de magia 🧙</p>
+        </div>
+        <div class="flex gap-4 col-span-2 items-center justify-end">
+          <a href="https://www.linkedin.com/in/paz-spera/" target="_blank" rel="noopener noreferrer" class="p-1 text-primary-100 hover:text-primary-400 transition ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-100  dark:focus-visible:ring-primary-400 active:text-primary-300 dark:active:text-primary-200"> <i class="devicon-linkedin-plain text-2xl"></i></a>
+          <a href="https://github.com/pazspera" target="_blank" rel="noopener noreferrer" class="p-1 text-primary-100 hover:text-primary-400 transition ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-100  dark:focus-visible:ring-primary-400 active:text-primary-300 dark:active:text-primary-200"> <i class="devicon-github-original text-2xl"></i></a>
         </div>
       </div>
     </div>  
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
+import "devicon/devicon.min.css";
 import App from "./App.vue";
 
 const savedTheme = localStorage.getItem("vueuse-color-scheme");
