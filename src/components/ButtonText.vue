d48f6f96060041346327c328eb01e527e383efc6
Paz Spera <mspera03@gmail.com>
2025-05-27 01:14:28 -0300

    Update tipografía botones en xl

--- src/components/ButtonText.vue
+++ src/components/ButtonText.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <a :href="props.href" class="uppercase px-4 py-2 rounded-sm text-primary-900 font-bold text-base hover:bg-gray-200/40 focus-visible:ring-2 focus-visible:ring-primary-500 
+  <a :href="props.href" class="uppercase px-4 py-2 rounded-sm text-primary-900 font-bold text-base lg:text-lg hover:bg-gray-200/40 focus-visible:ring-2 focus-visible:ring-primary-500 
   dark:focus-visible:ring-primary-700 focus-visible:outline-none active:bg-primary-900/20 transition">
     {{ props.label }}
   </a>
