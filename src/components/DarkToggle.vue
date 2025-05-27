766d9901d1d5b192e43010177128db563d948a87
Paz Spera <mspera03@gmail.com>
2025-05-26 23:38:19 -0300

    Agregada la imagen y cartel info para img. Maquetado para mobile

--- src/components/DarkToggle.vue
+++ src/components/DarkToggle.vue
@@ -12,4 +12,4 @@ const toggleDark = useToggle(isDark);
     <MoonIcon v-if="isDark" class="w-6 h-6 text-primary-100 hover:text-primary-400 transition ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-100  dark:focus-visible:ring-primary-400 active:text-primary-300 dark:active:text-primary-200" />
     <SunIcon v-else class="w-6 h-6 text-primary-100 hover:text-primary-400 transition ease-in-out" />
   </button>
-</template>
+</template> 
--- src/sections/Hero.vue
+++ src/sections/Hero.vue
@@ -4,9 +4,10 @@ import ButtonText from '../components/ButtonText.vue';
 </script>
 
 <template>
-  <section class="bg-primary-200 dark:bg-primary-400 dark:text-zinc-900 px-4 py-6">
+  <section class="bg-primary-200 dark:bg-primary-400 dark:text-zinc-900 px-4 pt-6 pb-18 rounded-br-[200]">
     <div class="container mx-auto">
       <div class="grid grid-cols-1 md:grid-cols-5 gap-6">
+        <!-- Texto -->
         <div class="md:col-span-3">
           <p class="text-sm">¡Hola, soy Paz! Me especializo en</p>
           <h1 class="font-title text-4xl font-bold mt-2 mb-4 text-primary-900">Captar la atención, guiar al usuario y lograr conversiones</h1>
@@ -14,6 +15,18 @@ import ButtonText from '../components/ButtonText.vue';
           <div class="mt-6 flex gap-6">
             <ButtonContained href="#" label="Ver proyectos"/>
             <ButtonText href="#" label="Contacto"/>
+          </div>
+        </div>
+        <!-- Img -->
+        <div class="flex items-center justify-center h-full">
+          <div class="relative w-3xs">
+            <img src="/src/assets/bio.png" alt="Paz Spera" class="w-full pb-2">
+            <div class="absolute bottom-0 w-full bg-gray-200 bg-opacity-60 text-black text-sm py-2 px-4 rounded-sm">
+              <div class="flex flex-col gap-1 items-center">
+                <span class="font-text">Diseñadora UX/UI</span>
+                <span class="font-text">Frontend Developer & Copywriter</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
