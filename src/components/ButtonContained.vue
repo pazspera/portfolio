f15962d827a1a038ea819828336356bc29000b51
Paz Spera <mspera03@gmail.com>
2025-05-26 22:49:28 -0300

    Agregado active, focus y dark active y focus en ButtonContained y ButtonText

--- src/components/ButtonContained.vue
+++ src/components/ButtonContained.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <a :href="props.href" class="uppercase px-4 py-2 rounded-sm bg-primary-500 text-primary-900 font-bold text-base hover:bg-primary-900 hover:text-primary-100 transition dark:bg-primary-900 dark:text-primary-100">
+  <a :href="props.href" class="uppercase px-4 py-2 rounded-sm bg-primary-500 text-primary-900 font-bold text-base hover:bg-primary-900 focus-visible:ring-2 focus-visible:ring-primary-700 focus-visible:outline-none  active:bg-primary-700 hover:text-primary-100 transition dark:bg-primary-900 dark:text-primary-100 dark:focus-visible:ring-primary-300">
     {{ props.label }}
   </a>
 </template>
--- src/components/ButtonText.vue
+++ src/components/ButtonText.vue
@@ -12,7 +12,8 @@ const props = defineProps({
 </script>
 
 <template>
-  <a :href="props.href" class="uppercase px-4 py-2 rounded-sm text-primary-900 font-bold text-base hover:bg-gray-200/40 transition">
+  <a :href="props.href" class="uppercase px-4 py-2 rounded-sm text-primary-900 font-bold text-base hover:bg-gray-200/40 focus-visible:ring-2 focus-visible:ring-primary-500 
+  dark:focus-visible:ring-primary-700 focus-visible:outline-none active:bg-primary-900/20 transition">
     {{ props.label }}
   </a>
 </template>
