e452e95ca999c75171aa4e867b0055d1707a9644
Paz Spera <mspera03@gmail.com>
2025-05-27 19:04:06 -0300

    Actualización estilo Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,8 +8,8 @@ let year = data.getFullYear();
     <div class="container mx-auto px-4 py-4 md:px-6 ">
       <div class="grid grid-cols-6 ">
         <div class="col-span-4">
-          <p class="text-base md:text-lg">&#169; {{ year }} - Paz Spera</p>
-          <p class="text-base md:text-lg">Hecho con ☕ + ❤️​ y un poco de magia 🧙</p>
+          <p class="text-base xl:text-lg">&#169; {{ year }} - Paz Spera</p>
+          <p class="text-base xl:text-lg">Hecho con ☕ + ❤️​ y un poco de magia 🧙</p>
         </div>
         <div class="flex gap-4 col-span-2 items-center justify-end">
           <a href="https://www.linkedin.com/in/paz-spera/" target="_blank" rel="noopener noreferrer" class="p-1 text-primary-100 hover:text-primary-400 transition ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-100  dark:focus-visible:ring-primary-400 active:text-primary-300 dark:active:text-primary-200"> <i class="devicon-linkedin-plain text-2xl"></i></a>
