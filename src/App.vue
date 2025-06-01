a778ded06eb40f81cee3556aee883bbd94cf6a64
Paz Spera <mspera03@gmail.com>
2025-06-01 19:34:57 -0300

    Agregado cambio de color ::selection

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import Contacto from "./sections/Contacto.vue";
 </script>
 
 <template>
-  <div class="font-text bg-light dark:bg-dark text-zinc-900 dark:text-zinc-200 transition-colors duration-500 ease-in-out" id="app">
+  <div class="font-text bg-light dark:bg-dark text-zinc-900 dark:text-zinc-200 selection:bg-primary-600 selection:text-zinc-950 dark:selection:bg-primary-200 transition-colors duration-500 ease-in-out" id="app">
     <Navbar />
     <main class="container mx-auto px-4 md:px-6 py-8">
       <Hero />
