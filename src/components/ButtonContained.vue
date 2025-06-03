dca05afd5af17c196fd6fb610b9e8fb8c3526ef0
Paz Spera <mspera03@gmail.com>
2025-06-03 16:01:07 -0300

    Corregido ButtonContained, no mostraba cursor pointer

--- src/components/ButtonContained.vue
+++ src/components/ButtonContained.vue
@@ -15,7 +15,7 @@ const type = props.type ?? "button";
   <a 
     v-if="href"
     :href="href"
-    class="flex justify-center items-center uppercase px-4 py-2 2xl:px-6 2xl:py-3 rounded-sm bg-primary-500 text-primary-900 font-bold text-base lg:text-lg xl:text-xl 2xl:text-2xl hover:bg-primary-900 focus-visible:ring-2 focus-visible:ring-primary-700 focus-visible:outline-none  active:bg-primary-700 hover:text-primary-100 transition dark:bg-primary-900 dark:hover:bg-primary-800 dark:text-primary-100 dark:focus-visible:ring-primary-300"
+    class="flex justify-center items-center uppercase px-4 py-2 2xl:px-6 2xl:py-3 rounded-sm bg-primary-500 text-primary-900 font-bold text-base lg:text-lg xl:text-xl 2xl:text-2xl hover:bg-primary-900 focus-visible:ring-2 focus-visible:ring-primary-700 focus-visible:outline-none  active:bg-primary-700 hover:text-primary-100 transition dark:bg-primary-900 dark:hover:bg-primary-800 dark:text-primary-100 dark:focus-visible:ring-primary-300 cursor-pointer"
     >
     <span>{{ label }}</span>
   </a>
@@ -25,7 +25,7 @@ const type = props.type ?? "button";
     v-else 
     :type="type" 
     :disabled="loading"
-    class="flex justify-center items-center uppercase px-4 py-2 2xl:px-6 2xl:py-3 rounded-sm bg-primary-500 text-primary-900 font-bold text-base lg:text-lg xl:text-xl 2xl:text-2xl hover:bg-primary-900 focus-visible:ring-2 focus-visible:ring-primary-700 focus-visible:outline-none  active:bg-primary-700 hover:text-primary-100 transition dark:bg-primary-900 dark:hover:bg-primary-800 dark:text-primary-100 dark:focus-visible:ring-primary-300 disabled:bg-primary-300 disabled:text-primary-700 disabled:cursor-not-allowed disabled:opacity-60"
+    class="flex justify-center items-center uppercase px-4 py-2 2xl:px-6 2xl:py-3 rounded-sm bg-primary-500 text-primary-900 font-bold text-base lg:text-lg xl:text-xl 2xl:text-2xl hover:bg-primary-900 focus-visible:ring-2 focus-visible:ring-primary-700 focus-visible:outline-none  active:bg-primary-700 hover:text-primary-100 transition dark:bg-primary-900 dark:hover:bg-primary-800 dark:text-primary-100 dark:focus-visible:ring-primary-300 disabled:bg-primary-300 disabled:text-primary-700 disabled:cursor-not-allowed disabled:opacity-60 cursor-pointer"
   >
     <span v-if="loading" class="flex items-center gap-2">
       <Loader/> Enviando...
