e99472860f9098ce26c236cf2d261310751846fb
Paz Spera <mspera03@gmail.com>
2025-06-03 16:00:00 -0300

    Agregado cambio de img on hover en ProjectCard

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,6 +2,7 @@
 import CardTitle from "../components/typography/CardTitle.vue";
 import MainText from "../components/typography/MainText.vue";
 import ButtonContained from "./ButtonContained.vue";
+import { ref } from "vue";
 
 type Project = {
   id: number,
@@ -17,11 +18,17 @@ defineProps<{
   project: Project
 }>();
 
+const isHovered = ref(false);
+
 </script>
 
 <template>
-  <div class="rounded-lg overflow-hidden">
-    <img :src="project.imgDefault" :alt="project.alt" class="w-full">
+  <article 
+    class="rounded-lg overflow-hidden transition-all duration-500"
+    @mouseenter="isHovered = true"
+    @mouseleave="isHovered = false"
+  >
+    <img :src="isHovered ? project.imgHover : project.imgDefault" :alt="project.alt" class="w-full h-auto object-cover transition-opacity duration-500">
     <div class="p-6 bg-slate-300 dark:bg-slate-700 dark:text-zinc-200">
       <CardTitle class="mb-2 text-primary-900 dark:text-primary-300">
         {{ project.title }}
@@ -31,5 +38,5 @@ defineProps<{
       </MainText>
       <ButtonContained label="Ver caso de estudio" class="mt-4" />
     </div>
-  </div>
+  </article>
 </template>
