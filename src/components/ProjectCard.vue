242bfb18e9022eaa9487b3c69e09368dc79e1bed
Paz Spera <mspera03@gmail.com>
2025-06-05 19:12:22 -0300

    Imágenes case study rediseño

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -25,12 +25,12 @@ const isHovered = ref(false);
 
 <template>
   <article 
-    class="rounded-lg overflow-hidden transition-all duration-500"
+    class="rounded-lg overflow-hidden transition-all duration-500 flex flex-col items-stretch"
     @mouseenter="isHovered = true"
     @mouseleave="isHovered = false"
   >
     <img :src="isHovered ? project.imgHover : project.imgDefault" :alt="project.alt" class="w-full h-auto object-cover transition-opacity duration-500">
-    <div class="p-6 bg-slate-300 dark:bg-slate-700 dark:text-zinc-200">
+    <div class="p-6 bg-slate-300 dark:bg-slate-700 dark:text-zinc-200 d-flex flex-col">
       <CardTitle class="mb-2 text-primary-900 dark:text-primary-300">
         {{ project.title }}
       </CardTitle>
