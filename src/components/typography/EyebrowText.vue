f439cc699dfaef3e2c49d6e92b0d21e0c5d12396
Paz Spera <mspera03@gmail.com>
2025-06-07 01:16:39 -0300

    Actualizado estilo en 2xl de HeroBlurb y EyebrownText

--- src/components/typography/EyebrowText.vue
+++ src/components/typography/EyebrowText.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <p class="text-sm lg:text-lg xl:text-xl">
+  <p class="text-sm lg:text-lg xl:text-xl 2xl:text-2xl">
     <slot></slot>
   </p>
 </template>
--- src/components/typography/HeroBlurb.vue
+++ src/components/typography/HeroBlurb.vue
@@ -1,7 +1,7 @@
 <script></script>
 
 <template>
-  <p class="text-lg/7 lg:text-xl xl:text-2xl">
+  <p class="text-lg/7 lg:text-xl xl:text-2xl 2xl:text-3xl">
     <slot></slot>
   </p>
 </template>
