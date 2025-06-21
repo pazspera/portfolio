f10e477ccd689670e8e2de1cd30701fb121d62bb
Paz Spera <mspera03@gmail.com>
2025-06-21 14:37:44 -0300

    Instalado splide en vez de vue-swipper, no estaba tomando la configuración de los breakpoints. También cambiado tsconfig.app.json que no tomaba bien los imports de splide

--- src/components/ImageSlider.vue
+++ src/components/ImageSlider.vue
@@ -1,48 +1,37 @@
 <script setup lang="ts">
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import 'swiper/swiper-bundle.css';
-import type { SwiperOptions } from 'swiper/types';
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import type { Options } from '@splidejs/splide';
 import type { SliderImage } from '../types/imageSlider';
-import { onMounted } from 'vue';
+import '@splidejs/vue-splide/css/default.css';
 
 defineProps<{
   images: SliderImage[];
 }>();
 
-const swiperOptions: SwiperOptions = {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  loop: true,
+const sliderOptions: Options = {
+  perPage: 1,
   breakpoints: {
     768: {
-      slidesPerView: 2,
-      spaceBetween: 10
+      perpPage: 2,
     },
     1200: {
-      slidesPerView: 3,
-      spaceBetween: 15,
+      perPage: 3,
     },
     1500: {
-      slidesPerView: 4,
-      spaceBetween: 20
+      perPage: 4,
     }
   }
 }
 
-onMounted(() => {
-  console.log("swiperoptions", swiperOptions);
-  console.log("tipo", typeof swiperOptions);
-  console.log("es array?", Array.isArray(swiperOptions))
-})
 
 </script>
 
 <template>
   <section class="py-6">
-    <Swiper :options="swiperOptions">
-      <SwiperSlide v-for="image in images" :key="image.id">
+    <Splide :options="sliderOptions">
+      <SplideSlide v-for="image in images" :key="image.id">
         <img :src="image.src" :alt="image.alt">
-      </SwiperSlide>
-    </Swiper>
+      </SplideSlide>
+    </Splide>
   </section>
 </template>
