086003e41b82c3456142f136ffdeaab3d2cf959a
Paz Spera <mspera03@gmail.com>
2025-07-09 16:50:34 -0300

    feat: Creada la thank you page y agregada la ruta. Agregado el composable para que muestre título personalizado

---
+++ src/pages/GraciasContactarse.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useDocumentTitle } from '../composables/useDocumentTitle';
+
+useDocumentTitle("Gracias por contactarse - Paz Spera")
+
+</script>
+
+<template>
+
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import RedisenoSitioInstitucional from "../pages/RedisenoSitioInstitucional.vue";
 import ContenidoRedes from "../pages/ContenidoRedes.vue";
+import GraciasContactarse from "../pages/GraciasContactarse.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "home", component: Home },
     { path: "/rediseno-institucional", name: "rediseno", component: RedisenoSitioInstitucional },
-    { path: "/contenido-redes", name: "redes", component: ContenidoRedes }
+    { path: "/contenido-redes", name: "redes", component: ContenidoRedes },
+    { path: "/gracias-por-contactarse", name: "gracias", component: GraciasContactarse }
   ],
   scrollBehavior(to, from, savedPosition) {
     if(to.hash) {
