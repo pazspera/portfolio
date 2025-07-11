7a64ede1d44cd456d318d6ef0497d0d137d8d51e
Paz Spera <mspera03@gmail.com>
2025-07-11 01:47:10 -0300

    fix(typography): Actualizado el line-height de CaptionText, era demasiado grande en mobile

--- src/components/typography/CaptionText.vue
+++ src/components/typography/CaptionText.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <p class="text-sm/7 lg:text-base/7 2xl:text-lg/7 mt-2">
+  <p class="text-sm/6 lg:text-base/6 2xl:text-lg/6 mt-2">
     <slot></slot>
   </p>
 </template>
