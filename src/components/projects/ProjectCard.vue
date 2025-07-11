21b350f9e911541bfb53f45364a8b82343e8f28c
Paz Spera <mspera03@gmail.com>
2025-07-11 18:02:53 -0300

    fix(ui): Corregí ProjectCard que el botón no quedaba alineado si el contenido de la card era diferente

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -31,14 +31,16 @@ const isHovered = ref(false);
     <div class="overflow-hidden relative transition-all duration-150 ease-in-out">
       <img :src="project.src" :alt="project.alt" :class="[isHovered ? 'scale-105' : 'scale-100' , 'w-full h-auto object-cover transition-all duration-300']">
     </div>  
-    <div class="p-6 bg-slate-300 dark:bg-slate-700 dark:text-zinc-200 d-flex flex-col flex-grow rounded-b-lg">
-      <TertiaryTitle class="mb-2 text-primary-900 dark:text-primary-300">
+    <div class="p-6 bg-slate-300 dark:bg-slate-700 dark:text-zinc-200 flex flex-col flex-grow place-items-between rounded-b-lg">
+      <div class="align-self-start">
+        <TertiaryTitle class="mb-2 text-primary-900 dark:text-primary-300">
         {{ project.title }}
-      </TertiaryTitle>
-      <MainText>
-        {{ project.text }}
-      </MainText>
-      <ButtonContained class="mt-4" size="small" :to="project.to"> 
+        </TertiaryTitle>
+        <MainText>
+          {{ project.text }}
+        </MainText>
+      </div>
+      <ButtonContained class="mt-4 align-self-end" size="small" :to="project.to"> 
         Ver caso de estudio
       </ButtonContained>
     </div>
