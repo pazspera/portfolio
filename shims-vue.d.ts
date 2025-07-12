1d2bc1efce62fb4798a6b14d5a3a57670b9a5f89
Paz Spera <mspera03@gmail.com>
2025-07-11 23:13:49 -0300

    fix: Instalé nueva versión de Splide, borré node_modules y package-lock.json y actualicé todo. También agregué configuración específica para el error de css en Splide. Quedan 2 errores

--- shims-vue.d.ts
+++ shims-vue.d.ts
@@ -12,3 +12,8 @@ declare module '*.scss' {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module '@splidejs/vue-splide/css' {
+  const content: string; // O Record<string, string>;
+  export default content;
+}
