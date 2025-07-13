4e8023103f6aaed615bea3bdf9e76129457de018
Paz Spera <mspera03@gmail.com>
2025-07-12 22:10:49 -0300

    chore: nuevo build con fixes y página 404

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -13,7 +13,7 @@ useDocumentTitle("Página no encontrada - Paz Spera")
     <section class="bg-primary-200 dark:bg-primary-400 dark:text-zinc-900 lg:w-[80%] lg:self-center px-4 sm:px-6 lg:px-8 pt-8 pb-14 md:pt-14 lg:pt-16 lg:pb-16 rounded-br-[120px] md:rounded-tl-[120px]">
       <div class="flex flex-col items-center justify-center text-center">
         <HeroTitle>¡Oh, no!</HeroTitle>
-        <div class="max-w-lg mx-auto">
+        <div class="max-w-2xl mx-auto">
           <HeroBlurb class="text-pretty">La página que estás buscando no está disponible o no existe. Dejemos que tenga su hipotética crisis existencial en paz.</HeroBlurb>
         </div>
         <ButtonContained class="mt-8 text-center" size="small" :to="{ name: 'home'}"> 
