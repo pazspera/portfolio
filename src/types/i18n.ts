ebef79efaee35976ff63a30dd3f5eadb41ed4fef
Paz Spera <mspera03@gmail.com>
2025-09-21 13:23:09 -0300

    chore: Creado el archivo i18n.ts que va a tener los tipos para la internacionalización. Lo creé para no olvidarme después que voy a usarlo

---
+++ src/types/i18n.ts
@@ -0,0 +1,10 @@
+/* 
+Acá van a ir los distintos tipos para internacionalización
+Por ahora es solo el MultilingualString que voy a necesitar en toda la app
+
+*/
+
+export type MultilingualString = {
+  es: string,
+  en: string,
+}
