22d3a97de4d942f758f9b8c2ee71137b8907f589
Paz Spera <mspera03@gmail.com>
2025-09-22 20:09:32 -0300

    refactor: Creado componente ProjectListSection para reemplazar los componentes ProjectImpacto y ProjectEnfoque. Ambos tienen la misma estructura y se pueden unificar

--- src/sections/projects/ProjectListSection.vue
+++ src/sections/projects/ProjectListSection.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
-import type { ItemEnfoque } from '../../types/projects';
+import { ContentItem } from '../../types/projects-v2';
 import SectionTitle from '../../components/typography/SectionTitle.vue';
 import ProjectTextCol from '../../components/projects/ProjectTextCol.vue';
 
-defineProps<{
-  itemsEnfoque: ItemEnfoque[],
+const props = defineProps<{
+  title: string,
+  items: ContentItem[],
 }>();
+
 </script>
 
 <template>
   <section class="py-6">
     <SectionTitle class="text-primary-900 dark:text-primary-300 mb-8">
-      Enfoque y metodología
+      {{ props.title }}
     </SectionTitle>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-      <ProjectTextCol v-for="item in itemsEnfoque" :key="item.id">
+      <ProjectTextCol v-for="item in props.items" :key="item.id">
         <template #title>
           {{ item.title }}
         </template>
