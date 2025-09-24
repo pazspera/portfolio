1905e3738567031ceafd23c74437bb2f9873b488
Paz Spera <mspera03@gmail.com>
2025-09-24 15:34:55 -0300

    refactor: Actualizado ImageCompare.vue para que use useImageUrl para armar la ruta de imágenes. Reformulada la manera en que se accede la info en el componente

--- src/components/ImageCompare.vue
+++ src/components/ImageCompare.vue
@@ -2,18 +2,24 @@
 import CaptionText from './typography/CaptionText.vue';
 import { VueCompareImage } from 'vue3-compare-image';
 import type { ImageCompareContent } from '../types/imageCompare';
+import { useImageUrl } from '../composables/useImageUrl';
 
-const props = defineProps<ImageCompareContent>();
+const props = defineProps<{
+  content: ImageCompareContent,
+}>();
+
+const { imgUrl: imgOldUrl } = useImageUrl(props.content.imgOld);
+const { imgUrl: imgNewUrl } = useImageUrl(props.content.imgNew);
 
 </script>
 
 <template>
   <section class="py-6">
       <VueCompareImage 
-        :leftImage="props.imgOld" 
-        :rightImage="props.imgNew" 
-        :leftImageAlt="props.altImgOld"
-        :rightImageAlt="props.altImgNew"
+        :leftImage="imgOldUrl" 
+        :rightImage="imgNewUrl" 
+        :leftImageAlt="props.content.altImgOld"
+        :rightImageAlt="props.content.altImgNew"
         :keyboard="true" 
         :keyboardStep="0.1"
         sliderLineColor="#033035"
@@ -21,7 +27,7 @@ const props = defineProps<ImageCompareContent>();
         :sliderPositionPercentage="0.5"
       />
       <CaptionText>
-        <slot></slot>
+        {{ props.content.caption }}
       </CaptionText>
     </section>
 </template>
