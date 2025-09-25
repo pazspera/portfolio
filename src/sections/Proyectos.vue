7f0487f1f1c4928aa1cdf047e6f2f0fdfa529d42
Paz Spera <mspera03@gmail.com>
2025-09-25 14:25:13 -0300

    refactor: Borrador array de proyectos que no se usa más

--- src/sections/Proyectos.vue
+++ src/sections/Proyectos.vue
@@ -3,35 +3,6 @@ import { caseStudies } from "../data/case-studies";
 import ProjectCard from "../components/projects/ProjectCard.vue";
 import SectionTitle from "../components/typography/SectionTitle.vue";
 
-const projects = [
-  {
-    id: 1,
-    title: "Rediseño de sitio institucional",
-    text: "Transformé la plataforma web de la empresa, implementando una nueva identidad de marca, reformulando la arquitectura de información y creando un sistema de diseño personalizado.",
-    // agregar ruta para btn
-    src: "/images/card-institutional-redesign.jpg",
-    alt: "Mockups de proyecto de rediseño de sitio institucional",
-    to: { name: 'rediseno' }
-  },
-  {
-    id: 2,
-    title: "Landing optimizada para ventas",
-    text: "Diseñé y desarrollé una landing page comercial para Liquid Sound, transformando su comunicación a un enfoque de ventas directo. El objetivo fue optimizar la captación de leads de alto valor y establecer una plataforma efectiva para campañas.",
-    src: "/images/card-landing-ventas.jpg",
-    alt: "Mockup de landing optimizada para ventas",
-    to: { name: "landing" }
-  },
-  {
-    id: 3,
-    title: "Contenido estratégico para redes",
-    text: "Desarrollé artículos y piezas gráficas para campañas de redes sociales de empresas B2B. Se buscó mejorar el engagement y posicionamiento orgánico, definiendo una voz de marca que conecte con el público objetivo. ",
-    // agregar ruta para btn
-    src: "/images/card-contenido-redes.jpg",
-    alt: "Mockups de publicaciones para redes sociales",
-    to: { name: 'redes' }
-  }
-]
-
 </script>
 
 <template>
