9c45b1e8137d28640754caa46f7dc0a7ee1e89a4
Paz Spera <mspera03@gmail.com>
2025-09-25 20:15:49 -0300

    refactor: Actualizados los tipos del componente NavLink

--- src/components/navbar/NavLink.vue
+++ src/components/navbar/NavLink.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-const props = defineProps({
-  to: {
-    type: String,
-    required: true
-  },
-})
+import type { RouteLocationRaw } from 'vue-router';
+
+defineProps<{
+  to: RouteLocationRaw,
+}>();
 </script>
 
 <template>
-  <RouterLink :to="props.to" class="py-2 px-3 text-base lg:text-lg xl:text-xl block uppercase font-semibold text-primary-100 hover:text-primary-400  hover:underline hover:underline-offset-8 decoration-2 transition ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-100  dark:focus-visible:ring-primary-400 active:text-primary-300 dark:active:text-primary-200">
+  <RouterLink :to="to" class="py-2 px-3 text-base lg:text-lg xl:text-xl block uppercase font-semibold text-primary-100 hover:text-primary-400  hover:underline hover:underline-offset-8 decoration-2 transition ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-100  dark:focus-visible:ring-primary-400 active:text-primary-300 dark:active:text-primary-200">
     <slot></slot>
   </RouterLink>
 </template>
