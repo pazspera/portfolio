eb061879771879add0e7c7076b840e5a409fd656
Paz Spera <mspera03@gmail.com>
2025-09-25 16:31:58 -0300

    feat: Agregada funcionalidad en SingleCaseStudy para que se carguen dinámicamente los proyectos con los slugs de las cards

--- src/pages/SingleCaseStudy.vue
+++ src/pages/SingleCaseStudy.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { caseStudies } from '../data/case-studies';
 import { useDocumentTitle } from "../composables/useDocumentTitle";
-import { computed } from 'vue';
+import { computed, ref, onMounted, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { Project } from '../types/projects-v2';
 
 import ProjectHero from '../sections/projects/ProjectHero.vue';
 import ProjectSummary from '../sections/projects/ProjectSummary.vue';
@@ -13,16 +15,9 @@ import ImageCompare from '../components/ImageCompare.vue';
 import ReadingProgressBar from "../components/ReadingProgressBar.vue";
 import ButtonContained from "../components/ButtonContained.vue";
 
-// el slug después va a venir de route.params.slug
-const slug = "contenido-redes"
-
-const project = caseStudies.find(c => c.slug === slug);
-
-// Manejar error
-// si no está que routee a página 404
-if(!project) {
-  console.log("Project not found");
-}
+const route = useRoute();
+const router = useRouter();
+const project = ref<Project | null>(null);
 
 // Objeto que une los strings de tipos de componente
 // con los componentes en sí mismos
@@ -37,13 +32,42 @@ const componentsMap = {
   ProjectImageCompare: ImageCompare,
 } as const;
 
-if(project) {
-  useDocumentTitle(project.title);
+const loadProject = (slug: string) => {
+  const currentProject = caseStudies.find(c => c.slug === slug);
+  
+  // Manejar error
+  // si no está que routee a página 404
+  if(!currentProject) {
+    console.log("Project not found");
+    router.replace({ name: "notFound" });
+    return;
+  }
+
+  project.value = currentProject;
+
+  useDocumentTitle(project.value.title);
 }
 
+// Carga inicial
+onMounted(() => {
+  const slugFromRoute = route.params.slug as string;
+  loadProject(slugFromRoute);
+})
+
+// Caso en que el usuario haga click en otro proyecto sin recargar
+watch (
+  () => route.params.slug,
+  (newSlug) => {
+    if(typeof newSlug === 'string') {
+      loadProject(newSlug);
+    }
+  }
+)
+
+
 const sections = computed(()=> {
-  if (!project) return [];
-  return project.sections as typeof project.sections;
+  if (!project.value) return [];
+  return project.value.sections;
 })
 
 </script>
