8d6a1fc4589c2214b1ba943712a85a21665417cd
Paz Spera <mspera03@gmail.com>
2025-10-01 02:35:19 -0300

    feature: Agregadas transiciones a la navegación entre rutas. Base tomada de la versión 0.2-legacy

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,11 @@ import Footer from './components/Footer.vue';
   <div class="overflow-x-hidden font-text bg-light dark:bg-dark text-zinc-900 dark:text-zinc-200 selection:bg-primary-600 selection:text-zinc-950 dark:selection:bg-primary-200 transition-colors duration-500 ease-in-out">
     <Navbar />
     <main class="container mx-auto px-4 md:px-6 pb-8 pt-24">
-      <RouterView />
+      <RouterView v-slot="{ Component, route }">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" :key="route.fullPath" />
+        </transition>
+      </RouterView>
     </main>
     <Footer />
   </div>
@@ -17,5 +21,19 @@ import Footer from './components/Footer.vue';
 <style>
 html.dark {
   color-scheme: dark;
+}
+.fade-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+.fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.fade-leave-active {
+  transition: all 0.3s ease-in;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,8 @@ const router = createRouter({
     if(to.hash) {
       return {
         el: to.hash,
+        left: 0,
+        top: 0,
         behavior: "smooth",
       };
     }
