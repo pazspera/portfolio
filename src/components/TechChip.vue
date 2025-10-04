a39a7a49d3ff13d0efad7bc98528ce31b72f784f
Paz Spera <mspera03@gmail.com>
2025-10-04 01:36:04 -0300

    perf: Agregado lazy loading a TechChip

--- src/components/TechChip.vue
+++ src/components/TechChip.vue
@@ -12,7 +12,7 @@ const { imgUrl } = useImageUrl(props.tech.src);
 
 <template>
   <div class="py-1.5 px-3 border border-gray-400 rounded-lg flex max-w-fit bg-transparent dark:bg-zinc-800 text-black dark:text-zinc-200 hover:opacity-85 transition">
-    <img :src="imgUrl" :alt="props.tech.alt" width="20px" height="20px" class="me-2">
+    <img :src="imgUrl" :alt="props.tech.alt" width="20px" height="20px" class="me-2" loading="lazy">
     <span>
       {{ props.tech.label }}
     </span>
